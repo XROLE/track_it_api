93c703a4d2877a7f1b35a86326e96e0a090f3de6
xrole <xrolediamond@gmail.com>
2024-09-14 23:03:53 +0100

    add lombok dependency configure spring security

---
+++ src/main/java/trackit/trackit/Security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package trackit.trackit.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.formLogin(httpForm -> {
+            httpForm.loginPage("/login").permitAll();
+        })
+        .authorizeHttpRequests(registry -> {
+            registry.requestMatchers("/api/v1/auth/**").permitAll();
+            registry.anyRequest().authenticated();
+        })
+                .build();
+    }
+
+}
